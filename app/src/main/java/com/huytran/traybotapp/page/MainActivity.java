b2f6daf3a9f67d2252a80f2f1c02289a28bbea11
macbookair <vuthiquynhchi72@gmail.com>
2025-07-08 21:37:43 +0700

    init app

---
+++ app/src/main/java/com/huytran/traybotapp/page/MainActivity.java
@@ -0,0 +1,16 @@
+package com.huytran.traybotapp.page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.huytran.traybotapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
