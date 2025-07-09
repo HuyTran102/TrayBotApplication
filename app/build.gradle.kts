65b9dadc3cb08883816389c9872ede3e84dc46c6
macbookair <vuthiquynhchi72@gmail.com>
2025-07-09 15:07:39 +0700

    add login UI to application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,10 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.compose.material3:material3:1.2.1") // nếu dùng Compose
+    implementation("com.google.android.material:material:1.9.0")// nếu dùng XML view
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.github.leandroborgesferreira:loading-button-android:2.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/huytran/traybotapp/page/login/LoginActivity.java
+++ app/src/main/java/com/huytran/traybotapp/page/login/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.huytran.traybotapp.page;
+package com.huytran.traybotapp.page.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.huytran.traybotapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
